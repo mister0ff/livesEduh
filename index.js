9285e5d95c12948fb510a194f96ae5e1143eed12
Eduhh <frazaoe620@gmail.com>
2026-09-20 22:04:41 -0300

    Update Firebase configuration and TikTok username

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ require('firebase/compat/database');
 
 // Suas credenciais do Firebase
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    databaseURL: "SUA_DATABASE_URL",
-    projectId: "SEU_PROJECT_ID"
+  apiKey: "AIzaSyDuKdQkPzGagnCIKUE5Yz_mKtpPw3OCf7c",
+  authDomain: "starcord-14470.firebaseapp.com",
+  databaseURL: "https://starcord-14470-default-rtdb.firebaseio.com",
+  projectId: "starcord-14470",
+  storageBucket: "starcord-14470.firebasestorage.app",
+  messagingSenderId: "154662528498",
+  appId: "1:154662528498:web:ff9815bb21dc2b4172776b",
+  measurementId: "G-QR4Y9ZHMF0"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-// SEU @ DO TIKTOK AQUI
-const tiktokUsername = "SEU_USUARIO_DO_TIKTOK";
+// COLQUE SEU @ DO TIKTOK AQUI (ex: "seu_usuario")
+const tiktokUsername = "SEU_USUARIO_DO_TIKTOK"; 
 const tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
 tiktokLiveConnection.connect().then(state => {
@@ -26,10 +31,9 @@ tiktokLiveConnection.connect().then(state => {
 tiktokLiveConnection.on('follow', data => {
     console.log(`Novo seguidor: ${data.uniqueId}`);
     
-    // Envia para o Firebase
+    // Envia os dados para o Firebase
     db.ref('seguidores/').push({
         nome: data.uniqueId,
         avatar: data.profilePictureUrl
     });
 });
-
